30e801d852f402e75a6d05114c383638df225867
xisijing <xisijing@gmail.com>
2015-06-14 23:57:36 +0200

    add includes() to RightOpenedInterval and implement the method

--- src/intervals/RightOpenedInterval.java
+++ src/intervals/RightOpenedInterval.java
@@ -5,5 +5,9 @@ public class RightOpenedInterval extends Interval {
 	public RightOpenedInterval(double minimum, double maximum) {
 		super(minimum, maximum);				
 	}
+	
+	public boolean includes(double value){
+		return getMinimum() <= value && value < getMaximum();
+	}
 
 }
