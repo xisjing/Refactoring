914339cb0ae9357406d01374ebfa1fcb0057234a
xisijing <xisijing@gmail.com>
2015-06-15 00:39:42 +0200

    change the includes interval to abstract method

--- src/intervals/Interval.java
+++ src/intervals/Interval.java
@@ -25,24 +25,10 @@ public abstract class Interval {
 	public double midPoint() {
 		return (this.maximum + this.minimum) / 2;
 	}
-
 	public abstract boolean includes(double value);
-
-	public boolean includes(Interval interval) {
-		
-		switch (interval.opening){
-					case LEFT_OPENED:
-						return ((this.includes(interval.minimum) || (interval.minimum==this.minimum)) && this.includes(interval.maximum));
-					case RIGHT_OPENED:
-						return ((this.includes(interval.maximum) || (interval.maximum==this.maximum)) && this.includes(interval.minimum));
-					case BOTH_OPENED:
-						return ((this.includes(interval.minimum) || (interval.minimum==this.minimum))) && ((this.includes(interval.maximum) || (interval.maximum==this.maximum)));
-					case UNOPENED:
-						return this.includes(interval.minimum) && this.includes(interval.maximum);
-				}
-				return false;
-	}
-
+	
+	public abstract boolean includes(Interval interval);
+	
 	public boolean intersectsWith(Interval interval) {
 		// TODO
 		return false;
