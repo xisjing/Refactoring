f08d038228bbde585d0968e29ce1c1ff77e87686
xisijing <xisijing@gmail.com>
2015-06-15 00:26:15 +0200

    implement the includes interval in class BothOpenedInterval

--- src/intervals/BothOpenedInterval.java
+++ src/intervals/BothOpenedInterval.java
@@ -9,6 +9,10 @@ public class BothOpenedInterval extends Interval {
 	public boolean includes(double value){
 		return getMinimum() < value && value < getMaximum();
 	}
+	
+	public boolean includes(Interval interval){
+		return ((this.includes(interval.getMinimum()) || (interval.getMinimum()==this.getMinimum()))) && ((this.includes(interval.getMaximum()) || (interval.getMaximum()==this.getMaximum())));
+	}
 
 
 }
