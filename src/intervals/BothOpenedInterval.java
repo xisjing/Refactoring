ed9fa8c1c9d3d23d4d02601ac2b952085287a8bd
xisijing <xisijing@gmail.com>
2015-06-14 23:59:11 +0200

    add includes() to BothOpenedInterval and implement the method

--- src/intervals/BothOpenedInterval.java
+++ src/intervals/BothOpenedInterval.java
@@ -5,6 +5,10 @@ public class BothOpenedInterval extends Interval {
 	public BothOpenedInterval(double minimum, double maximum) {
 		super(minimum, maximum);				
 	}
+	
+	public boolean includes(double value){
+		return getMinimum() < value && value < getMaximum();
+	}
 
 
 }
