fd5c137ce74c03f262d713cea3a569b047be88ca
xisijing <xisijing@gmail.com>
2015-06-15 00:00:35 +0200

    add includes() to UnOpenedInterval and implement the method

--- src/intervals/UnOpenedInterval.java
+++ src/intervals/UnOpenedInterval.java
@@ -6,6 +6,10 @@ public class UnOpenedInterval extends Interval {
 	public UnOpenedInterval(double minimum, double maximum) {
 		super(minimum, maximum);				
 	}
+	
+	public boolean includes(double value){
+		return getMinimum() <= value && value <= getMaximum();
+	}
 
 
 }
